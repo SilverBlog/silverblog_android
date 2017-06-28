f3a765904336a4fd1ebac35625411c0873aaca10
qwe7002(workstation) <qwe7002@hotmail.com>
2017-06-28 21:26:46 +0800

    修改向缪名称

--- app/src/main/java/reallct/qwe7002/smartblog_client/main_Activity.java
+++ app/src/main/java/reallct/qwe7002/smartblog_client/main_Activity.java
@@ -20,18 +20,18 @@ public class main_Activity extends AppCompatActivity {
         Button save_button = (Button) findViewById(R.id.save_button);
         Button new_post_button = (Button) findViewById(R.id.send_button);
         Button edit_post_button = (Button) findViewById(R.id.edit_button);
-        sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
         final EditText host = (EditText) findViewById(R.id.host);
         final EditText password = (EditText) findViewById(R.id.password);
+        sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
         host.setText(sharedPreferences.getString("host", null));
         password.setText(sharedPreferences.getString("password", null));
         new_post_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (host.getText().length()!= 0&&password.getText().length()!= 0) {
+                if (host.getText().length() != 0 && password.getText().length() != 0) {
                     Intent new_post_activity = new Intent(main_Activity.this, new_post_Activity.class);
                     startActivity(new_post_activity);
-                }else{
+                } else {
                     Snackbar.make(view, "请先配置服务器信息", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
@@ -40,10 +40,10 @@ public class main_Activity extends AppCompatActivity {
         edit_post_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (host.getText().length()!= 0&&password.getText().length()!= 0) {
+                if (host.getText().length() != 0 && password.getText().length() != 0) {
                     Intent edit_post_activity = new Intent(main_Activity.this, post_list_Activity.class);
                     startActivity(edit_post_activity);
-                }else{
+                } else {
                     Snackbar.make(view, "请先配置服务器信息", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
                 }
@@ -52,12 +52,17 @@ public class main_Activity extends AppCompatActivity {
         save_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                SharedPreferences.Editor editor = sharedPreferences.edit();
-                editor.putString("host", String.valueOf(host.getText()));
-                editor.putString("password", String.valueOf(password.getText()));
-                editor.apply();
-                Snackbar.make(view, "配置已保存", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (host.getText().length() != 0 && password.getText().length() != 0) {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("host", String.valueOf(host.getText()));
+                    editor.putString("password", String.valueOf(password.getText()));
+                    editor.apply();
+                    Snackbar.make(view, "配置已保存", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                } else {
+                    Snackbar.make(view, "请先配置服务器信息", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
             }
         });
     }
