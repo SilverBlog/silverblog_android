97e6ea699e08166a488aabd70ea978669d166007
qwe7002(workstation) <qwe7002@hotmail.com>
2017-07-08 13:40:57 +0800

    修改判断实现

--- app/src/main/java/reallct/qwe7002/smartblog_client/main_Activity.java
+++ app/src/main/java/reallct/qwe7002/smartblog_client/main_Activity.java
@@ -16,6 +16,7 @@ public class main_Activity extends AppCompatActivity {
     SharedPreferences sharedPreferences;
     String host_save;
     String password_save;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,13 +37,14 @@ public class main_Activity extends AppCompatActivity {
                 if (host_save != null && password_save != null) {
                     Intent new_post_activity = new Intent(main_Activity.this, post_Activity.class);
                     startActivity(new_post_activity);
-                } else {
-                    if (getCurrentFocus() != null && getCurrentFocus().getWindowToken() != null) {
-                        manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-                    }
-                    Snackbar.make(view, "请先配置服务器信息", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    return;
                 }
+                if (getCurrentFocus() != null && getCurrentFocus().getWindowToken() != null) {
+                    manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+                Snackbar.make(view, "请先配置服务器信息", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
             }
         });
         edit_post_button.setOnClickListener(new View.OnClickListener() {
@@ -51,13 +53,14 @@ public class main_Activity extends AppCompatActivity {
                 if (host_save != null && password_save != null) {
                     Intent edit_post_activity = new Intent(main_Activity.this, post_list_Activity.class);
                     startActivity(edit_post_activity);
-                } else {
-                    if (getCurrentFocus() != null && getCurrentFocus().getWindowToken() != null) {
-                        manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
-                    }
-                    Snackbar.make(view, "请先配置服务器信息", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    return;
                 }
+                if (getCurrentFocus() != null && getCurrentFocus().getWindowToken() != null) {
+                    manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+                Snackbar.make(view, "请先配置服务器信息", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
             }
         });
         save_button.setOnClickListener(new View.OnClickListener() {
@@ -66,20 +69,21 @@ public class main_Activity extends AppCompatActivity {
                 if (getCurrentFocus() != null && getCurrentFocus().getWindowToken() != null) {
                     manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                 }
-                if (host.getText().length() != 0 && password.getText().length() != 0) {
+                if (host.getText().length() == 0 && password.getText().length() == 0) {
+                    Snackbar.make(view, "请先配置服务器信息", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    return;
+                }
                     String hosturl = String.valueOf(host.getText());
                     SharedPreferences.Editor editor = sharedPreferences.edit();
                     editor.putString("host", String.valueOf(hosturl));
                     editor.putString("password", silverblog_connect.getMD5(String.valueOf(password.getText())));
                     editor.apply();
-                    host_save=String.valueOf(hosturl);
-                    password_save=silverblog_connect.getMD5(String.valueOf(password.getText()));
+                    host_save = String.valueOf(hosturl);
+                    password_save = silverblog_connect.getMD5(String.valueOf(password.getText()));
                     Snackbar.make(view, "配置已保存", Snackbar.LENGTH_LONG)
                             .setAction("Action", null).show();
-                } else {
-                    Snackbar.make(view, "请先配置服务器信息", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
+
             }
         });
     }
