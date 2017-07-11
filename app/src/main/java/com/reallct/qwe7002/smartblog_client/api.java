cd3b82fe5136ac1f42260ce0a3f4e418bf34eea9
qwe7002(workstation) <qwe7002@hotmail.com>
2017-07-11 23:37:15 +0800

    修改包名，修改二维码差错

---
+++ app/src/main/java/com/reallct/qwe7002/smartblog_client/api.java
@@ -0,0 +1,78 @@
+package com.reallct.qwe7002.smartblog_client;
+
+import com.squareup.okhttp.MediaType;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.concurrent.TimeUnit;
+
+
+class api {
+
+    private static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    static String getMD5(String source) {
+        String mdString = null;
+        if (source != null) {
+            try {
+                mdString = getBytes(source.getBytes("UTF-8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+        return mdString;
+    }
+
+    private static String getBytes(byte[] source) {
+        String s = null;
+        char[] hexDigits = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
+                '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        final int temp = 0xf;
+        final int arraySize = 32;
+        final int strLen = 16;
+        final int offset = 4;
+        try {
+            java.security.MessageDigest md = java.security.MessageDigest
+                    .getInstance("MD5");
+            md.update(source);
+            byte[] tmp = md.digest();
+            char[] str = new char[arraySize];
+            int k = 0;
+            for (int i = 0; i < strLen; i++) {
+                byte byte0 = tmp[i];
+                str[k++] = hexDigits[byte0 >>> offset & temp];
+                str[k++] = hexDigits[byte0 & temp];
+            }
+            s = new String(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
+
+    static String send_request(String url, String json, String method) {
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url + "/control/" + method)
+                .post(body)
+                .build();
+        OkHttpClient okHttpClient = new OkHttpClient();
+        okHttpClient.setConnectTimeout(30, TimeUnit.SECONDS);
+        okHttpClient.setReadTimeout(30, TimeUnit.SECONDS);
+        String result = "{\"status\":false}";
+        try {
+            Response response = okHttpClient.newCall(request).execute();
+            if (response.code() == 200) {
+                result = response.body().string();
+            }
+
+        } catch (IOException ignored) {
+        }
+        return result;
+    }
+}
---
+++ app/src/main/java/com/reallct/qwe7002/smartblog_client/main_Activity.java
@@ -0,0 +1,156 @@
+package com.reallct.qwe7002.smartblog_client;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import com.google.zxing.activity.CaptureActivity;
+
+
+public class main_Activity extends AppCompatActivity {
+    SharedPreferences sharedPreferences;
+    String host_save;
+    String password_save;
+    EditText host;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.maintoolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (ContextCompat.checkSelfPermission(main_Activity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(main_Activity.this, new String[]{Manifest.permission.CAMERA}, 1);
+                    return false;
+                }
+                Intent intent = new Intent(main_Activity.this, CaptureActivity.class);
+                startActivityForResult(intent, 0);
+                return true;
+            }
+        });
+        Button save_button = (Button) findViewById(R.id.save_button);
+        Button edit_post_button = (Button) findViewById(R.id.edit_button);
+        host = (EditText) findViewById(R.id.host);
+        final EditText password = (EditText) findViewById(R.id.password);
+        sharedPreferences = getSharedPreferences("data", MODE_PRIVATE);
+        host_save = sharedPreferences.getString("host", null);
+        password_save = sharedPreferences.getString("password", null);
+        host.setText(host_save);
+        final InputMethodManager manager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        edit_post_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (host_save != null && password_save != null) {
+                    Intent edit_post_activity = new Intent(main_Activity.this, post_list_Activity.class);
+                    startActivity(edit_post_activity);
+                    return;
+                }
+                if (getCurrentFocus() != null && getCurrentFocus().getWindowToken() != null) {
+                    manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+                Snackbar.make(view, "请先配置服务器信息", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getCurrentFocus() != null && getCurrentFocus().getWindowToken() != null) {
+                    manager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+                if (host.getText().length() == 0 || password.getText().length() == 0) {
+                    Snackbar.make(view, "请先配置服务器信息", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    return;
+                }
+                String hosturl = String.valueOf(host.getText());
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("host", hosturl);
+                editor.putString("password", api.getMD5(String.valueOf(password.getText())));
+                editor.apply();
+                host_save = String.valueOf(hosturl);
+                password_save = api.getMD5(String.valueOf(password.getText()));
+                Snackbar.make(view, "配置已保存", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(main_Activity.this, CaptureActivity.class);
+                    startActivityForResult(intent, 0);
+                    return;
+                }
+                Snackbar.make(host, "请授权相机权限，以便能够正常扫描二维码。", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                break;
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) { //RESULT_OK = -1
+            Bundle bundle = data.getExtras();
+            String scanResult = bundle.getString("result");
+            JsonParser parser = new JsonParser();
+            JsonObject objects;
+            try {
+                 objects = parser.parse(scanResult).getAsJsonObject();
+            }catch(IllegalStateException e){
+                Snackbar.make(host, "二维码错误！", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                return;
+            }
+            String hosturl = objects.get("url").getAsString();
+            String password=objects.get("password").getAsString();
+            if(password.length()==0||hosturl.length()==0){
+                Snackbar.make(host, "请检查您的System.json配置文件。", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("host", hosturl);
+            editor.putString("password", password);
+            editor.apply();
+            host.setText(hosturl);
+            Snackbar.make(host, "配置已保存", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+
+        }
+    }
+}
