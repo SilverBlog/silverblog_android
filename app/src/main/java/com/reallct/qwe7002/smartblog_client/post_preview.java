0d020df939947e70fa06c399e2dcbab141b334dd
qwe7002 <qwe7002@hotmail.com>
2017-09-26 18:40:54 +0800

    修改排版

--- app/src/main/java/com/reallct/qwe7002/smartblog_client/post_preview.java
+++ app/src/main/java/com/reallct/qwe7002/smartblog_client/post_preview.java
@@ -17,7 +17,7 @@ public class post_preview extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         Intent intent = getIntent();
-        RichText.fromMarkdown("# " + intent.getStringExtra(Intent.EXTRA_SUBJECT) + "\n" + intent.getStringExtra(Intent.EXTRA_TEXT)).into((TextView) findViewById(R.id.markdown_view));
+        RichText.fromMarkdown("# " + intent.getStringExtra(Intent.EXTRA_SUBJECT) + "\n\n" + intent.getStringExtra(Intent.EXTRA_TEXT)).into((TextView) findViewById(R.id.markdown_view));
 
     }
 
