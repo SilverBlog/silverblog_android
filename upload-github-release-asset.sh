00d12c9a72b2b7f8bd1097b555977ad2df929afd
qwe7002 <qwe7002@hotmail.com>
2018-03-07 20:29:59 +0800

    修改编译脚本

--- upload-github-release-asset.sh
+++ upload-github-release-asset.sh
@@ -41,7 +41,8 @@ AUTH="Authorization: token $github_api_token"
 WGET_ARGS="--content-disposition --auth-no-challenge --no-cookie"
 CURL_ARGS="-LJO#"
 
-curl -H "Authorization: token $github_api_token" -X POST -d '{"tag_name":"'$tag'"}' "$GH_REPO/releases" 
+curl -H "Authorization: token $github_api_token" -X POST -d '{"tag_name":"'$tag'","body":"'$body'"}' "$GH_REPO/releases"
+
 if [[ "$tag" == 'LATEST' ]]; then
   GH_TAGS="$GH_REPO/releases/latest"
 fi
